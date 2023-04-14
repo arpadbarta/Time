dd3b5b716a63d283f77a13daca9ebaad97540f2d
Árpád Barta <arpad.barta@outlook.com>
2023-04-14 10:57:12 +0300

    Adds github and date time formatting links

---
+++ Time/Links.cs
@@ -0,0 +1,11 @@
+﻿using Time.Services;
+
+namespace Time
+{
+    public class Links
+    {
+        public static UrlDefinition Github { get; } = new(nameof(Github), "https://github.com/arpadbarta/Time");
+        public static UrlDefinition Coffee { get; } = new(nameof(Coffee), "https://www.buymeacoffee.com/arpad.barta");
+        public static UrlDefinition DateTimeDocs { get; } = new(nameof(DateTimeDocs), "https://learn.microsoft.com/en-us/dotnet/standard/base-types/standard-date-and-time-format-strings#table-of-format-specifiers");
+    }
+}
---
+++ Time/Services/ProcessService.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using Microsoft.AppCenter.Analytics;
+
+namespace Time.Services
+{
+    public record UrlDefinition(string Name, string Path);
+
+    public class ProcessService
+    {
+        public static void Open(UrlDefinition definition)
+        {
+            Analytics.TrackEvent($"opened-{definition.Name}");
+
+            _ = Process.Start(new ProcessStartInfo(definition.Path) { UseShellExecute = true });
+        }
+    }
+}
